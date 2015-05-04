163b7c5fd70c7c7a4ad99ec9d5d58eda2c6825e1
Mike Kroutikov <pgmmpk@gmail.com>
2015-05-04 15:20:33 -0400

    copied dev mongo configuration to test (so that jenkins test pass) not sure why package.json has been changed

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/adcappuccino-test'
+      uri: 'mongodb://dev:dev@ds031822.mongolab.com:31822/dev'
   }
 };
