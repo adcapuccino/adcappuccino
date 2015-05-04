1707e3b6f5308ad1cf00bc0ee520eb4089b1eb10
Mike Kroutikov <pgmmpk@gmail.com>
2015-05-04 10:55:16 -0400

    re-generated with mongodb (login workflow was generated broken without mongo). now should work locally in dev mode (via mongolab): grunt serve

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/adcappuccino-dev'
+    uri: 'mongodb://dev:dev@ds031822.mongolab.com:31822/dev'
   },
 
   seedDB: true
